b7f1abeb738477b3f086dcff98928099bc6f7947
Muhammed Enes Şahin <40808896+freeadmiral@users.noreply.github.com>
2019-01-14 01:43:51 +0300

    sort-icon

--- src/components/common/TableHeader.jsx
+++ src/components/common/TableHeader.jsx
@@ -11,12 +11,23 @@ class TableHeader extends Component {
         }
         this.props.onSorted(sortedItem);
     };
+
+    renderSortIcon=column=>{
+        const {sortedItem}=this.props;
+        if(column.path!==sortedItem.path) return null;
+        if(sortedItem.order==='asc') return <i className="fa fa-sort-asc"></i>
+        return <i className="fa fa-sort-desc"></i>;
+    };
+
     render() { 
         return (
         <thead>
             <tr>
                 {this.props.columns.map(column =>(
-                    <th key={column.path || column.key} onClick={()=>this.raiseSort(column.path)}>{column.label}</th>
+                    <th key={column.path || column.key}
+                     onClick={()=>this.raiseSort(column.path)}>
+                     {column.label} {this.renderSortIcon(column)}
+                    </th>
                 ))}        
             </tr>
         </thead>
