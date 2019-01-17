b40eeaa030d899ffcc57f06b61509287d04c892e
sss <fdsf@sfd.com>
2019-01-17 17:44:33 +0300

    reusable input

--- src/components/common/LoginForm.jsx
+++ src/components/common/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Input from './Input';
 
 class LoginForm extends Component {
 
@@ -24,44 +25,11 @@ class LoginForm extends Component {
       <React.Fragment>
         <h1 style={{ textAlign: "center" }}>Login</h1>
         <form className="container" onSubmit={this.handleSubmit} >
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              name="username"
-              value={account.username}
-              onChange={this.handleChange}
-              autoFocus
-              type="text"
-              className="form-control"
-              id="username"
-              placeholder="Enter username"
-            />
-            <small id="emailHelp" className="form-text text-muted">
-              We'll never share your email with anyone else.
+          <Input name="username" value={account.username} onChange={this.handleChange} label="Username" />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
             </small>
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              name="password"
-              value={account.password}
-              onChange={this.handleChange}
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Enter password"
-            />
-          </div>
-          <div className="form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-            />
-            <label className="form-check-label" htmlFor="exampleCheck1">
-              Check me out
-            </label>
-          </div>
+          <Input name="password" value={account.password} onChange={this.handleChange} label="Password" />
           <button type="submit" className="btn btn-primary">
             Login
           </button>
