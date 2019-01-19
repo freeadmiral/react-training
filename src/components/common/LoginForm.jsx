21be78c46198051e0e86a63b7e708332f61bd4c8
Muhammed Enes Şahin <40808896+freeadmiral@users.noreply.github.com>
2019-01-20 01:15:00 +0300

    reusable form

--- src/components/common/LoginForm.jsx
+++ src/components/common/LoginForm.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React  from "react";
 import Input from './Input';
 import Joi from 'joi-browser';
+import Form from './Form';
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     errors:{}
   };
 
@@ -13,56 +14,22 @@ class LoginForm extends Component {
     password : Joi.string().required().label('Password')
   };
 
-  validate=()=>{
-    const options = {abortEarly:false};
-    const result = Joi.validate(this.state.account,this.schema,options);
-    console.log(result);  
-    if(!result.error) return null;
-
-    const errors = {};
-    for(let item of result.error.details) errors[item.path[0]] = item.message;
-    return errors;
-  };
-
-  validateProperty = ({name,value}) => {
-   const obj={[name]:value};
-   const schema={ [name]:this.schema[name] };
-   const {error}= Joi.validate(obj,schema);
-   return error ? error.details[0].message : null;
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = {...this.state.errors};
-    const errorMessage = this.validateProperty(input);
-    if(errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({ account,errors });    
-  };
-
-
-  handleSubmit = e => {
-    e.preventDefault(); // full page reload
-    const errors = this.validate();
-    this.setState({errors:errors || {}});
-    if(errors) return;
+  goSubmit = () =>{
     console.log('submitted');
   };
 
   
   render() {
-    const { account,errors } = this.state;
+    const { data,errors } = this.state;
     return (
       <React.Fragment>
         <h1 style={{ textAlign: "center" }}>Login</h1>
         <form className="container" onSubmit={this.handleSubmit} >
-          <Input placeholder="Enter username" name="username" value={account.username} onChange={this.handleChange} errors={errors.username} label="Username" />
+          <Input placeholder="Enter username" name="username" value={data.username} onChange={this.handleChange} errors={errors.username} label="Username" />
           <small id="emailHelp" className="form-text text-muted">
             We'll never share your email with anyone else.
             </small >
-          <Input placeholder="Enter password" name="password" value={account.password} onChange={this.handleChange} errors={errors.password} label="Password" />
+          <Input placeholder="Enter password" name="password" value={data.password} onChange={this.handleChange} errors={errors.password} label="Password" />
           <button disabled={this.validate()} type="submit" className="btn btn-primary">
             Login
           </button>
