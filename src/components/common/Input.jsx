8381f021c6462520f9eb295665476740627171d7
Muhammed Enes Şahin <40808896+freeadmiral@users.noreply.github.com>
2019-01-20 00:57:53 +0300

    validation with JOI

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
-const Input = ({ name, label, value, onChange }) => {
+const Input = ({ name, label, value, onChange,placeholder,errors }) => {
     return (
         <div className="form-group">
             <label htmlFor={name}>{label}</label>
             <input
-                name="username"
+                name={name}
                 value={value}
                 onChange={onChange}
                 type="text"
                 className="form-control"
                 id={name}
-                placeholder="Enter username"
+                placeholder={placeholder}
+                errors={errors}
             />
-
+           {errors && <div className="alert alert-danger">{errors}</div>
+}
         </div>);
 }
 
--- src/components/common/LoginForm.jsx
+++ src/components/common/LoginForm.jsx
@@ -1,35 +1,69 @@
 import React, { Component } from "react";
 import Input from './Input';
+import Joi from 'joi-browser';
 
 class LoginForm extends Component {
   state = {
-    account: { username: "", password: "" }
+    account: { username: "", password: "" },
+    errors:{}
   };
 
-  handleSubmit = e => {
-    e.preventDefault(); // full page reload
+  schema ={
+    username : Joi.string().required().label('Username'),
+    password : Joi.string().required().label('Password')
+  };
 
-    console.log('submitted');
+  validate=()=>{
+    const options = {abortEarly:false};
+    const result = Joi.validate(this.state.account,this.schema,options);
+    console.log(result);  
+    if(!result.error) return null;
+
+    const errors = {};
+    for(let item of result.error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  validateProperty = ({name,value}) => {
+   const obj={[name]:value};
+   const schema={ [name]:this.schema[name] };
+   const {error}= Joi.validate(obj,schema);
+   return error ? error.details[0].message : null;
   };
 
   handleChange = ({ currentTarget: input }) => {
+    const errors = {...this.state.errors};
+    const errorMessage = this.validateProperty(input);
+    if(errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account,errors });    
   };
 
+
+  handleSubmit = e => {
+    e.preventDefault(); // full page reload
+    const errors = this.validate();
+    this.setState({errors:errors || {}});
+    if(errors) return;
+    console.log('submitted');
+  };
+
+  
   render() {
-    const { account } = this.state;
+    const { account,errors } = this.state;
     return (
       <React.Fragment>
         <h1 style={{ textAlign: "center" }}>Login</h1>
         <form className="container" onSubmit={this.handleSubmit} >
-          <Input name="username" value={account.username} onChange={this.handleChange} label="Username" />
+          <Input placeholder="Enter username" name="username" value={account.username} onChange={this.handleChange} errors={errors.username} label="Username" />
           <small id="emailHelp" className="form-text text-muted">
             We'll never share your email with anyone else.
             </small >
-          <Input name="password" value={account.password} onChange={this.handleChange} label="Password" />
-          <button type="submit" className="btn btn-primary">
+          <Input placeholder="Enter password" name="password" value={account.password} onChange={this.handleChange} errors={errors.password} label="Password" />
+          <button disabled={this.validate()} type="submit" className="btn btn-primary">
             Login
           </button>
         </form>
